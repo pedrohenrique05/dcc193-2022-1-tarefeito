ce8b594fa2bcf0b7552d0d63c4e56255db9bbd52
Pedro Moura <pedrohenriquedelgado@ice.ufjf.br>
2022-07-07 20:43:41 -0300

    tudo ok

--- src/main/java/br/dcc193/tarefeito/Tarefa.java
+++ src/main/java/br/dcc193/tarefeito/Tarefa.java
@@ -4,14 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 
 @Entity
 public class Tarefa {
     
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
-    private Long id;
+    private Long id; 
+    @NotBlank(message = "E preciso um titulo1")
     private String titulo;
+    @PositiveOrZero(message = "Tomatos tem que ser 0 ou maior")
     private Integer tomatos;
     
     public Tarefa(Long id, String titulo, Integer tomatos) {
--- src/main/java/br/dcc193/tarefeito/TarefaController.java
+++ src/main/java/br/dcc193/tarefeito/TarefaController.java
@@ -2,10 +2,15 @@ package br.dcc193.tarefeito;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,54 +18,104 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/tarefa")
 public class TarefaController {
+
     @Autowired
     private TarefaRepository rep;
 
     @RequestMapping("/")
-    @ResponseBody //A saida dessa função retorna direto ao cliente
-    public String indexOld(){
-        Tarefa t = new Tarefa("Criado em "+new Date());
+    @ResponseBody // A saida dessa função retorna direto ao cliente
+    public String indexOld() {
+        Tarefa t = new Tarefa("Criado em " + new Date());
         rep.save(t);
-        
+
         List<Tarefa> tarefas = rep.findAll();
         StringBuilder sb = new StringBuilder();
         sb.append("<h1>Olá mundo!</h1>");
-        for(Tarefa tarefa : tarefas){
+        for (Tarefa tarefa : tarefas) {
             sb.append("<li>" + tarefa);
         }
         return sb.toString();
     }
 
-    @RequestMapping({"/tarefa","/tarefa/index.html"})
-    public ModelAndView index(){
+    @RequestMapping({ "/", "/index.html" })
+    public ModelAndView index() {
         ModelAndView mv = new ModelAndView("tarefa-index");
-        mv.addObject("mensagem","Ola Mundo");
+        mv.addObject("mensagem", "Ola Mundo");
         return mv;
     }
 
-    @GetMapping("/tarefa/nova.html")
-    public ModelAndView novaGET(){
+    @GetMapping("/nova.html")
+    public ModelAndView novaGET() {
         ModelAndView mv = new ModelAndView("tarefa-new");
-        Tarefa t = new Tarefa("Criado em "+new Date());
+        Tarefa t = new Tarefa("Criado em " + new Date());
         mv.addObject("tarefa", t);
         return mv;
     }
 
-    @PostMapping("/tarefa/nova.html")
-    public ModelAndView novaPOST(Tarefa t){
+    @PostMapping("/nova.html")
+    public ModelAndView novaPOST(@Valid Tarefa t, BindingResult binding) {
         ModelAndView mv = new ModelAndView("tarefa-new");
+        if (binding.hasErrors()) {
+            mv.setViewName("tarefa-new.html");
+            mv.addObject("tarefa", t);
+            return mv;
+        }
         rep.save(t);
         mv.addObject("tarefa", t);
         mv.setViewName("redirect:./listar.html");
         return mv;
     }
 
-    @GetMapping(path = "/tarefa/listar.html")
-    public ModelAndView listar(){
+    @GetMapping(path = "/listar.html")
+    public ModelAndView listar() {
         ModelAndView mv = new ModelAndView("tarefa-list");
         List<Tarefa> tl = rep.findAll();
         mv.addObject("tarefas", tl);
         return mv;
     }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView editarGET(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("tarefa-edit");
+
+        Optional<Tarefa> tarefaop = rep.findById(id);
+        if (tarefaop.isPresent()) {
+            Tarefa t = tarefaop.get();
+            mv.setViewName("tarefa-edit");
+            mv.addObject("tarefa", t);
+            return mv;
+        }
+        mv.setViewName("redirect:../listar.html");
+        return mv;
+    }
+
+    @PostMapping("/editar/{id}")
+    public ModelAndView editarPOST(@Valid Tarefa t, BindingResult binding) {
+        ModelAndView mv = new ModelAndView();
+        if (binding.hasErrors()) {
+            mv.setViewName("terefa-edit.html");
+            mv.addObject("tarefa", t);
+            return mv;
+        }
+        rep.save(t);
+        mv.setViewName("redirect:../listar.html");
+        return mv;
+
+    }
+
+    @GetMapping("/excluir/{id}")
+    public ModelAndView excluir(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("redirect:../listar.html");
+
+        Optional<Tarefa> tarefaop = rep.findById(id);
+
+        if (tarefaop.isPresent()) {
+            Tarefa t = tarefaop.get();
+            rep.delete(t);
+        }
+        return mv;
+    }
+
 }
